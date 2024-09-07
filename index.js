17ffdd365c93b0ff9e8c6dfc2f41356772bc0706
zainali28 <zainali123113@gmail.com>
2024-09-08 03:35:48 +0500

    added moving windows (need improvement)

--- index.js
+++ index.js
@@ -5,7 +5,43 @@ function logKey(e) {
     screenLog.setAttribute(
         "style", `top: ${e.clientY-25}px; left: ${e.clientX-25}px;`
     );
-//   screenLog.innerText = `
-//     Screen X/Y: ${e.screenX}, ${e.screenY}
-//     Client X/Y: ${e.clientX}, ${e.clientY}`;
+}
+
+let rectangle = document.querySelector(".rectangle");
+let currentX = 0;
+let currentY = 0;
+let isDragging = false;
+document.addEventListener("mousedown", holdButton);
+document.addEventListener("mousemove", onMove);
+document.addEventListener("mouseup", releaseButton);
+
+function holdButton(e) {
+    currentX = e.clientX;
+    currentY = e.clientY;
+    isDragging = true;
+    console.log(`Initial X: ${currentX}; Initial Y: ${currentY}`);
+}
+
+function releaseButton(e) {
+    isDragging = false;
+    currentX = e.clientX;
+    currentY = e.clientY;
+    console.log(`Final X: ${currentX}; Final Y: ${currentY}`);
+    rectangle.setAttribute(
+        "style", `top: ${e.clientY}px; left: ${e.clientX}px;`
+    );
+}
+
+function onMove(e) {
+    if (isDragging) {
+        console.log(rectangle);
+        let positionX = `${e.clientX - currentX}px`;
+        let positionY = `${e.clientY - currentY}px`;
+
+        console.log(`Middle X: ${e.clientX}; Middle Y: ${e.clientY}`);
+
+        rectangle.setAttribute(
+            "style", `top: ${e.clientY}px; left: ${e.clientX}px;`
+        );
+    }
 }
